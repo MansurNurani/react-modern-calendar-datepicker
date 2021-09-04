d6869242c6fb3ba4552cada4e4f0a54ac504344c
MansurNurani <mansur.nurani@gmail.com>
2021-09-04 10:11:30 +0430

    test

--- playground/src/index.js
+++ playground/src/index.js
@@ -2,12 +2,27 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import '../../src/DatePicker.css';
-import DatePicker from '../../src';
+import DatePicker, { Calendar } from '../../src';
 import * as serviceWorker from './serviceWorker';
 
 const App = () => {
   const [selectedDay, setValue] = useState(null);
-  return <DatePicker value={selectedDay} onChange={setValue} shouldHighlightWeekends />;
+  const [selectedDay4, setValue4] = useState(null);
+  return (
+    <>
+      <DatePicker value={selectedDay} onChange={setValue} shouldHighlightWeekends locale="fa" />
+      <hr />
+      <Calendar
+        value={selectedDay4}
+        onChange={setValue4}
+        calendarClassName="fontWrapper -persian"
+        shouldHighlightWeekends
+        locale="fa"
+      />
+    </>
+    // <>
+    // </>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
